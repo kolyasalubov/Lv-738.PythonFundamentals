c90e2e21d853ce84ea91104875834ffc232b156a
Kateryna Bebeshko <katrin.bebeshko@gmail.com>
2023-01-20 21:26:25 +0100

    HW 1-3

---
+++ HW1/task1.py
@@ -0,0 +1,8 @@
+userName = input('What is your name?: ')
+userAge = input('How old are you?: ')
+userLocation = input('Where do you live?: ')
+
+print(f"Hello, {userName}. Your age is {userAge}. You live in {userLocation}.")
+
+
+
