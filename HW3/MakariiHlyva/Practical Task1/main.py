2d0e853ee467227ced7d75a6396b5d4a21ad8095
mojoym <mojoym@gmail.com>
2023-01-22 23:20:06 +0200

    HW3 1-st task ready

---
+++ HW3/MakariiHlyva/Practical Task1/main.py
@@ -0,0 +1,22 @@
+#---------------Work with string of Zen of Python file----------------------------
+
+file = open("python.txt", "r")
+data_from_file = file.read()
+
+# Find quantity of different word is file
+quantity_of_better = data_from_file.find('better')
+quantity_of_never = data_from_file.find('never')
+quantity_of_is = data_from_file.find("is")
+
+# Printing results
+print(f"Quantity of word 'better' in Zen of Python is - {quantity_of_better}")
+print(f"Quantity of word 'never' in Zen of Python is - {quantity_of_never}")
+print(f"Quantity of word 'is' in Zen of Python is - {quantity_of_is}")
+
+# Print all text in uppercase
+uppercase_text = data_from_file.upper()
+print(f"\nPrint all text in uppercase: \n\n{uppercase_text}")
+
+# Replase all symbols "i" with "&"
+replaced_text = data_from_file.replace('i', '&')
+print(f"\n\nText with replaced symbols i:\n\n{replaced_text}")
