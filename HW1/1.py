986832b794a0fb3c133ab56eb2903aa8a73bce2e
Taras Maksymiv <tmax7miv@gmail.com>
2023-01-24 20:42:19 +0200

    Added HW from lessons 1, 2 and 3

---
+++ HW1/1.py
@@ -0,0 +1,11 @@
+name = input("What is your name?\n").capitalize()
+age = int(input("How old are you?\n"))
+city = input("Where do you live?\n").capitalize()
+
+print("Hello,", name)
+print("Your age is", age)
+print("You live in", city)
+
+#--------------------v2------------------
+
+#print(f"Hello, {name}\nYour age is {age}\nYou live in {city}")
