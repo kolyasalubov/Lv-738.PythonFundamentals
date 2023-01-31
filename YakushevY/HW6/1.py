d51b94ec7fb32f97405e71fe03025495532431cd
Yurii Yakushev <megalover23@gmail.com>
2023-01-31 17:26:29 +0200

    update HW6/1.py

--- YakushevY/HW6/1.py
+++ YakushevY/HW6/1.py
@@ -1,5 +1,5 @@
 divisible_by_two = [i for i in range(1,10) if i%2==0]
-divisible_by_three = [i for i in range(1,10) if i%3==0]
+divisible_by_three = [i for i in range(1,10) if i%3==0 and i%2!=0]
 not_divisible = [i for i in range(1,10) if i%2!=0 and i%3!=0]
 
 print("Number divisible by two: ",divisible_by_two)
@@ -17,7 +17,7 @@ for i in a:
 
 
 for i in a:
-    if i%3==0:
+    if i%3==0 and i%2!=0:
         print (i)
 
 
