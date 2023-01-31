0b5b8ba6eeae7ad8a3c73995c78204206151045a
Ruden Maksym <maxrudjob@gmail.com>
2023-01-31 14:43:42 +0000

    fix HW3 task2

--- Ruden/HW3/2.py
+++ Ruden/HW3/2.py
@@ -1,4 +1,9 @@
 n1 = input("enter a four-digit natural number: ")
+num_str = str(n1)
+product = 1
+for char in num_str:
+        product = product * int(char)
+print('product of digits: ', product) 
 n_list = list(n1)
 n_list.reverse()
 n2 = "".join(n_list)
