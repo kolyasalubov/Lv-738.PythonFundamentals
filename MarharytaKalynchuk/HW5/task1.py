c15ff5b1d7be0b39aa69a77199ff6751fe194182
Kalynchuk Marharyta <marharytakalynchuk@gmail.com>
2023-02-04 22:18:58 +0200

    added HW5

---
+++ MarharytaKalynchuk/HW5/task1.py
@@ -0,0 +1,17 @@
+ints = [2, 7, 9, 5, 7]
+floats = []
+
+for value in ints:
+    floats.append(float(value))
+print (floats)
+
+# lst = [2,34,23,867,433,32]
+# for i in range(len(lst)):
+#     lst[i] = float(lst[i])
+# print(lst)
+
+# ints = [2, 7, 8, 9, 8]
+
+# floats = [float(x) for x in ints] 
+
+# print(floats)
---
+++ MarharytaKalynchuk/HW5/task2.py
@@ -0,0 +1,11 @@
+
+def fibo():
+    n = int(input('Please enter your desired number: '))
+    fibo_list = [0, 1]
+    i = 2
+    while i<n:
+        fibo_list.append(fibo_list[i-2]+fibo_list[i-1])
+        i += 1
+    print(fibo_list)
+
+fibo()
