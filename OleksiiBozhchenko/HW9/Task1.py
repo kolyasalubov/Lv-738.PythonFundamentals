5fe55e6e87d550dfd90758142d244912c840311c
Oleksii Bozhchenko <a.bozhchenko@gmail.com>
2023-02-08 23:37:46 +0200

    added HW9 (Task1 and Task2)

--- OleksiiBozhchenko/HW9/Task1.py
+++ OleksiiBozhchenko/HW9/Task1.py
@@ -8,28 +8,6 @@
 # # then the corresponding message is displayed. (to perform the task, you need to import the random module,
 # # and from it the randint() function)
 
-# import random
-
-# number = random.randint(1, 40)
-# counter = 0
-
-# while counter <= 9:
-#     counter = counter + 1
-#     if counter <= 9:
-#         print(f"Attempt number {counter}.")
-#     else:
-#         print("You have one last attempt!")
-#     user_number = int(input("Enter you number: "))
-#     if user_number == number:
-#         print(f"Congratulations! The number {number} was guessed")
-#         break
-#     elif 1 <= user_number <= 40 and user_number < number:
-#         print("Your number is less than expected")
-#     elif 1 <= user_number <= 40 and user_number > number:
-#         print("Your number is more than expected")
-#     elif not 1 <= user_number <= 40:
-#         print("Your number is not in range 1 to 40")
-
 import random
 
 number = random.randint(1, 40)
