9d1a120d65b0c45f241edd193370abc4d2e26a73
Oleksii Bozhchenko <a.bozhchenko@gmail.com>
2023-02-09 13:30:53 +0200

    added HW9/Task3 and ClassWork

--- OleksiiBozhchenko/HW9/Task3.py
+++ OleksiiBozhchenko/HW9/Task3.py
@@ -1,25 +1,58 @@
 from pyowm import OWM
+import tkinter as tk
+from tkinter import font
 
 API_KEY = 'ef2206ff5da67de63306d0b143e20872'
+HEIGHT = 350
+WIDTH = 450
 
-owm = OWM(API_KEY)
-mgr = owm.weather_manager()
+def weather_response():
+ try:
+    owm = OWM(API_KEY)
+    mgr = owm.weather_manager()
 
-city = input("Please enter your city : ")
-observation = mgr.weather_at_place(city)
-w = observation.weather
+    city = entry_field.get()
+    observation = mgr.weather_at_place(city)
 
-w.detailed_status         # 'clouds'
-w.wind()                  # {'speed': 4.6, 'deg': 330}
-w.humidity                # 87
-w.temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
-w.rain                    # {}
-w.heat_index              # None
-w.clouds                  # 75
+    w = observation.weather
 
-print(f"""
-The weather in {city} today is:
-Temperature : {w.temperature('celsius')['temp']} 'C
-Humidity : {w.humidity} %
-Clouds : {w.clouds} %
-""")
+    out = str(f"""
+    City: {city}
+    Conditions: {w.detailed_status}
+    Temperature is {round(w.temperature('celsius')['temp'], 2)} 'C
+    Wind speed is {w.wind()['speed']} km/hours
+    Humidity of the air is {w.humidity} %
+    """)
+    return out
+ except:
+    return('''Oops!!! There was a problem\nretrieving that information.''')
+
+def get_weather():
+  label['text'] = weather_response()
+  
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+root.title("Weather Application")
+canvas.pack()
+
+frame = tk.Frame(root, bg="deep sky blue", bd=5)
+frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+
+entry_field = tk.Entry(frame, font=('Courier', 12))
+entry_field.place(relx=0, rely=0, relwidth=0.65, relheight=1)
+
+button = tk.Button(frame, 
+                   text="Get Weather", 
+                   bg="gray", fg="white", 
+                   font=('Courier', 8), 
+                   command=lambda: get_weather())
+button.place(relx=0.7, rely=0, relwidth=0.3, relheight=1)
+
+lower_frame = tk.Frame(root, bg='gold', bd=10)
+lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+
+label = tk.Label(lower_frame, font=('Courier', 14))
+label.place(relx=0, rely=0, relwidth=1, relheight=1)
+
+root.mainloop()
