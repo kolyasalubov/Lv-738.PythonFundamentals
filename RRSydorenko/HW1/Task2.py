0eb0724179a25fa820940ef19bd3296674ba604e
Sydorenko Roman <roman.romanovy4@gmail.com>
2023-02-16 18:42:04 +0200

    Added new files in HW1

---
+++ RRSydorenko/HW1/Task2.py
@@ -0,0 +1,11 @@
+print("Task_2")
+
+print(" ")
+
+val_a = int(input ("Enter a value A = "))
+val_b = int(input ("Enter a value B = "))
+print ("a + b = ", val_a + val_b)
+print ("a - b = ", val_a - val_b)
+print ("a * b = ", val_a * val_b)
+print ("a / b = ", val_a / val_b)
+print ("a ** b = ", val_a ** val_b)
