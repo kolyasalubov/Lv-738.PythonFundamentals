041472665f6a26e0b31d1d8b1219549a59d3e8df
Andrii Vasyliv <vasuliv62@gmail.com>
2023-02-21 12:35:04 +0200

    added new HW4, HW5, HW6

--- AndriiVasyliv/HW8/calculator.py
+++ AndriiVasyliv/HW8/calculator.py
@@ -1,7 +1,13 @@
-def sum(num1 + num2):
+def sum(num1, num2):
     return num1 + num2
 
+def minus(num1, num2):
+    return num1 - num2
 
-    return sum
-    sum = a + b
-    print()
+def product(num1, num2):
+    return num1 * num2
+
+def division(num1, num2):
+    return num1 / num2
+
+  
--- AndriiVasyliv/HW8/operations.py
+++ AndriiVasyliv/HW8/operations.py
@@ -1,6 +1,9 @@
 import calculator
 
-num1 = input(int())
-num2 = input(int())
+num1 = int(input("Enter first number "))
+num2 = int(input("Enter second number "))
 
-print(calculator.add(num1, num2))
+print(calculator.sum(num1, num2))
+print(calculator.minus(num1, num2))
+print(calculator.product(num1, num2))
+print(calculator.division(num1, num2))
