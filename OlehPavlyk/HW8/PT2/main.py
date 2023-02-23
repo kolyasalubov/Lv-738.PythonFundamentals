43666c3fa62b35321d6d7c89f5b2fed0c54e07c7
Oleh Pavlyk <pavluk.oleg37@gmail.com>
2023-02-23 09:29:02 +0200

    add new homework

--- OlehPavlyk/HW8/PT2/main.py
+++ OlehPavlyk/HW8/PT2/main.py
@@ -1,4 +1,5 @@
-password = input("create a password\n")
-if len(password) >= 6 | len(password) <= 16:
-    print(len(password))
-    lenn = True
+# password = input("create a password\n")
+# if len(password) >= 6 | len(password) <= 16:
+#     print(len(password))
+#     lenn = True4
+
