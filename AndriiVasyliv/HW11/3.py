5ad796d732dc06d525cfd242953f334b2a446015
Andrii Vasyliv <vasuliv62@gmail.com>
2023-03-14 20:03:43 +0200

    added new HW9, HW11, HW13, My project

--- AndriiVasyliv/HW11/3.py
+++ AndriiVasyliv/HW11/3.py
@@ -4,13 +4,15 @@ try:
         a, b = input("Enter two numbers, separated by a comma: ").split()
         a = int()
         print('try succeed')      
-    except IndexError:
+except IndexError:
         result = 0
         print('Index out of range')
-    except:        # run if other exception is raised
+except:        # run if other exception is raised
         result = 0
         print('other exception')
-    else:          # run if no exception raised
+else:          # run if no exception raised
         print('no exception raised')
-    finally:       # always run regardless of whether exception is raised
+finally:       # always run regardless of whether exception is raised
         print('run finally')
+        
+        
